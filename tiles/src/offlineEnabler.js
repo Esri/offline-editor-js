d7e2537fdb57b5dca86aba500f25cb94d6856d13
Javier Abadia <javi.abadia@gmail.com>
2013-12-04 23:52:12 +0100

    fixed implementation of base4 encoding

--- tiles/src/offlineEnabler.js
+++ tiles/src/offlineEnabler.js
@@ -85,73 +85,6 @@ define([
 
 				layer.storeTile = function(level,row,col,callback)
 				{
-					function customBase64Encode (inputStr) 
-					{
-					    var
-					        bbLen               = 3,
-					        enCharLen           = 4,
-					        inpLen              = inputStr.length,
-					        inx                 = 0,
-					        jnx,
-					        keyStr              = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-					                            + "0123456789+/=",
-					        output              = "",
-					        paddingBytes        = 0;
-					    var
-					        bytebuffer          = new Array (bbLen),
-					        encodedCharIndexes  = new Array (enCharLen);
-
-					    while (inx < inpLen) {
-					        for (jnx = 0;  jnx < bbLen;  ++jnx) {
-					            /*--- Throw away high-order byte, as documented at:
-					              https://developer.mozilla.org/En/Using_XMLHttpRequest#Handling_binary_data
-					            */
-					            if (inx < inpLen)
-					                bytebuffer[jnx] = inputStr.charCodeAt (inx++) & 0xff;
-					            else
-					                bytebuffer[jnx] = 0;
-					        }
-
-					        /*--- Get each encoded character, 6 bits at a time.
-					            index 0: first  6 bits
-					            index 1: second 6 bits
-					                        (2 least significant bits from inputStr byte 1
-					                         + 4 most significant bits from byte 2)
-					            index 2: third  6 bits
-					                        (4 least significant bits from inputStr byte 2
-					                         + 2 most significant bits from byte 3)
-					            index 3: forth  6 bits (6 least significant bits from inputStr byte 3)
-					        */
-					        encodedCharIndexes[0] = bytebuffer[0] >> 2;
-					        encodedCharIndexes[1] = ( (bytebuffer[0] & 0x3) << 4)   |  (bytebuffer[1] >> 4);
-					        encodedCharIndexes[2] = ( (bytebuffer[1] & 0x0f) << 2)  |  (bytebuffer[2] >> 6);
-					        encodedCharIndexes[3] = bytebuffer[2] & 0x3f;
-
-					        //--- Determine whether padding happened, and adjust accordingly.
-					        paddingBytes          = inx - (inpLen - 1);
-					        switch (paddingBytes) {
-					            case 1:
-					                // Set last character to padding char
-					                encodedCharIndexes[3] = 64;
-					                break;
-					            case 2:
-					                // Set last 2 characters to padding char
-					                encodedCharIndexes[3] = 64;
-					                encodedCharIndexes[2] = 64;
-					                break;
-					            default:
-					                break; // No padding - proceed
-					        }
-
-					        /*--- Now grab each appropriate character out of our keystring,
-					            based on our index array and append it to the output string.
-					        */
-					        for (jnx = 0;  jnx < enCharLen;  ++jnx)
-					            output += keyStr.charAt ( encodedCharIndexes[jnx] );
-					    }
-					    return output;
-					}
-
 					var url = this._getTileUrl(level,row,col);
 					var store = this.offline.store;
 
@@ -159,17 +92,13 @@ define([
 					var imgurl = "proxy.php?" + url;
 					var req = new XMLHttpRequest();
 					req.open("GET", imgurl, true);
-					/* https://hacks.mozilla.org/2012/02/storing-images-and-files-in-indexeddb/ */
-					//req.responseType == "blob";
-					/**/
 					req.overrideMimeType("text/plain; charset=x-user-defined"); // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest?redirectlocale=en-US&redirectslug=DOM%2FXMLHttpRequest%2FUsing_XMLHttpRequest#Handling_binary_data 
-					//*/
+
 					req.onload = function()
 					{
 						if( req.status == 200 )
 						{							
-							//var img = this.response;
-							var img = customBase64Encode(this.responseText);
+							var img = Base64Utils.wordToBase64(Base64Utils.stringToWord(this.responseText));
 
 							var tile = {
 								url: url,
