d2b4daa6c4de6b61cf8cf541cde50094a1bc512c
Javier Abadia <javi.abadia@gmail.com>
2013-12-17 00:18:16 +0100

    started writing some unit tests

--- test/jasmine/spec/offlineEnablerSpec.js
+++ test/jasmine/spec/offlineEnablerSpec.js
@@ -2,14 +2,22 @@
 
 describe("offline enabler library", function()
 {
+    it("validate map", function()
+    {
+        expect(g_map.id).toEqual("map");
+        expect(g_map).toEqual(jasmine.any(Object));
+    });
+
 	it("extends the tiled layer object", function()
 	{
-
+		expect(g_basemapLayer.goOffline).toBeUndefined();
+		g_offlineEnabler.extend(g_basemapLayer);
+		expect(g_basemapLayer.goOffline).not.toBeUndefined();
 	});
 
 	it("can go offline", function()
 	{
-
+		
 	});
 
 	it("delete all tiles", function()
