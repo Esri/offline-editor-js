842f35a2b1f69573a164f945aa3d4bf03810220e
Javier Abadia <javi.abadia@gmail.com>
2014-01-30 18:25:59 +0100

    unit testing for offlineFeaturesManager

---
+++ test/spec/offlineFeaturesManagerSpec.js
@@ -0,0 +1,70 @@
+"use strict";
+
+describe("Normal online editing", function()
+{
+	describe("Clear Feature Layers", function()
+	{		
+		it("deletes all features - points", function()
+		{
+
+		});
+
+		it("deletes all features - lines", function()
+		{
+
+		});
+
+		it("deletes all features - polygons", function()
+		{
+
+		});
+	});
+
+	describe("Add features", function()
+	{
+	});
+
+	describe("Update features", function()
+	{
+	});
+
+	describe("Delete features", function()
+	{
+	});
+}
+
+describe("Offline Editing", function()
+{
+	describe("Prepare Feature Layers", function()
+	{		
+	});
+
+	describe("Go Offline", function()
+	{
+	});
+
+	describe("Update existing features", function()
+	{
+	});
+	
+	describe("Delete existing features", function()
+	{
+	});
+
+	describe("Add new features", function()
+	{
+	});
+
+	describe("Update new features", function()
+	{
+	});
+	
+
+	describe("Delete new features", function()
+	{
+	});
+
+	describe("Go Online", function()
+	{
+	});
+})
