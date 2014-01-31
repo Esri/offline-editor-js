ca3a0fe806e571d3994a6ec85bacfc7b28d5166c
Javier Abadia <javi.abadia@gmail.com>
2014-01-31 01:00:01 +0100

    good progress in unit testing

--- test/spec/offlineFeaturesManagerSpec.js
+++ test/spec/offlineFeaturesManagerSpec.js
@@ -1,70 +1,444 @@
 "use strict";
 
-describe("Normal online editing", function()
+/*
+ * helper functions
+ */
+
+function clearFeatureLayer(featureLayer, cb)
 {
-	describe("Clear Feature Layers", function()
-	{		
-		it("deletes all features - points", function()
-		{
-
-		});
-
-		it("deletes all features - lines", function()
-		{
-
-		});
-
-		it("deletes all features - polygons", function()
-		{
-
-		});
-	});
-
-	describe("Add features", function()
+	g_modules.esriRequest({
+		url: featureLayer.url + "/deleteFeatures",
+		content: { f: 'json', where: '1=1'},
+		handleAs: 'json'
+	},{usePost:true}).then( function(response)
 	{
-	});
-
-	describe("Update features", function()
+		cb && cb(true,response);
+	},
+	function(error)
 	{
-	});
-
-	describe("Delete features", function()
-	{
+		cb && cb(false,error);
 	});
 }
 
+function countFeatures(featureLayer, cb)
+{
+	g_modules.esriRequest({
+		url: featureLayer.url + "/query",
+		content: { f: 'json', where: '1=1', returnCountOnly:true},
+		handleAs: 'json'
+	},{usePost:true}).then( function(response)
+	{
+		cb && cb(true,response);
+	},
+	function(error)
+	{
+		cb && cb(false,error);
+	});
+}
+
+/*
+ * tests begin here
+ */
+var async = new AsyncSpec(this);
+
+describe("Normal online editing", function()
+{
+	var g1,g2,g3;
+
+	describe("Original applyEdits method", function()
+	{
+		async.it("clears the feature layers", function(done)
+		{
+			var count = 0;
+			function completedOne()
+			{
+				count += 1;
+				if(count==3)
+					done();
+			}
+			clearFeatureLayer( g_featureLayers[0], function(success,response)
+			{
+				expect(success).toBeTruthy();
+				var listener = g_featureLayers[0].on('update-end', function(){ listener.remove(); completedOne();})
+				g_featureLayers[0].refresh();
+				
+			});
+			clearFeatureLayer( g_featureLayers[1], function(success,response)
+			{
+				expect(success).toBeTruthy();
+				var listener = g_featureLayers[1].on('update-end', function(){ listener.remove(); completedOne();})
+				g_featureLayers[1].refresh();
+			});
+			clearFeatureLayer( g_featureLayers[2], function(success,response)
+			{
+				expect(success).toBeTruthy();
+				var listener = g_featureLayers[2].on('update-end', function(){ listener.remove(); completedOne();})
+				g_featureLayers[2].refresh();
+			});
+		});
+
+		async.it("add test features", function(done)
+		{
+			expect(g_featureLayers[0].graphics.length).toBe(0);
+
+			g1 = new g_modules.Graphic({"geometry":{"x":-105400,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+			g2 = new g_modules.Graphic({"geometry":{"x":-105600,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+			g3 = new g_modules.Graphic({"geometry":{"x":-105800,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+
+			var adds = [g1,g2,g3];
+			g_featureLayers[0]._applyEdits(adds,null,null,function(addResults,updateResults,deleteResults)
+			{
+				expect(addResults.length).toBe(3);
+				expect(addResults[0].success).toBeTruthy();
+				expect(addResults[1].success).toBeTruthy();
+				expect(addResults[2].success).toBeTruthy();
+				g1.attributes.objectid = addResults[0].objectId;
+				g2.attributes.objectid = addResults[1].objectId;
+				g3.attributes.objectid = addResults[2].objectId;
+				expect(g_featureLayers[0].graphics.length).toBe(3);
+				countFeatures(g_featureLayers[0], function(success,result)
+				{
+					expect(success).toBeTruthy();
+					expect(result.count).toBe(3);
+					done();					
+				});
+			},
+			function(error)
+			{
+				expect(true).toBeFalsy();
+				done();
+			});
+		});
+
+		async.it("update test features", function(done)
+		{
+			expect(g_featureLayers[0].graphics.length).toBe(3);
+
+			g1.geometry.y += 300;
+			g2.geometry.y += 100;
+			g3.geometry.y -= 200;
+			var updates = [g1,g2,g3];
+			g_featureLayers[0]._applyEdits(null,updates,null,function(addResults,updateResults,deleteResults)
+			{
+				expect(updateResults.length).toBe(3);
+				expect(updateResults[0].success).toBeTruthy();
+				expect(updateResults[1].success).toBeTruthy();
+				expect(updateResults[2].success).toBeTruthy();
+				expect(g_featureLayers[0].graphics.length).toBe(3);
+				done();
+			},
+			function(error)
+			{
+				expect(true).toBeFalsy();
+				done();
+			});
+		});
+
+		async.it("delete test features", function(done)
+		{
+			expect(g_featureLayers[0].graphics.length).toBe(3);
+
+			var deletes = [g3];
+			g_featureLayers[0]._applyEdits(null,null,deletes,function(addResults,updateResults,deleteResults)
+			{
+				expect(deleteResults.length).toBe(1);
+				expect(g_featureLayers[0].graphics.length).toBe(2);
+				done();
+			},
+			function(error)
+			{
+				expect(true).toBeFalsy();
+				done();
+			});
+		});
+	});
+
+	describe("Extended applyEdits method (online)", function()
+	{
+		async.it("clears the feature layers", function(done)
+		{
+			var count = 0;
+			function completedOne()
+			{
+				count += 1;
+				console.log(count);
+				if(count==3)
+					done();
+			}
+			clearFeatureLayer( g_featureLayers[0], function(success,response)
+			{
+				expect(success).toBeTruthy();
+				var listener = g_featureLayers[0].on('update-end', function(){ listener.remove(); completedOne();})
+				g_featureLayers[0].refresh();
+				
+			});
+			clearFeatureLayer( g_featureLayers[1], function(success,response)
+			{
+				expect(success).toBeTruthy();
+				var listener = g_featureLayers[1].on('update-end', function(){ listener.remove(); completedOne();})
+				g_featureLayers[1].refresh();
+			});
+			clearFeatureLayer( g_featureLayers[2], function(success,response)
+			{
+				expect(success).toBeTruthy();
+				var listener = g_featureLayers[2].on('update-end', function(){ listener.remove(); completedOne();})
+				g_featureLayers[2].refresh();
+			});
+		});
+
+		async.it("add test features", function(done)
+		{
+			expect(g_featureLayers[0].graphics.length).toBe(0);
+
+			g1 = new g_modules.Graphic({"geometry":{"x":-105400,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+			g2 = new g_modules.Graphic({"geometry":{"x":-105600,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+			g3 = new g_modules.Graphic({"geometry":{"x":-105800,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+
+			var adds = [g1,g2,g3];
+			g_featureLayers[0].applyEdits(adds,null,null,function(addResults,updateResults,deleteResults)
+			{
+				expect(addResults.length).toBe(3);
+				expect(addResults[0].success).toBeTruthy();
+				expect(addResults[1].success).toBeTruthy();
+				expect(addResults[2].success).toBeTruthy();
+				g1.attributes.objectid = addResults[0].objectId;
+				g2.attributes.objectid = addResults[1].objectId;
+				g3.attributes.objectid = addResults[2].objectId;
+				console.log(g_featureLayers[0].graphics);
+				expect(g_featureLayers[0].graphics.length).toBe(3);
+				countFeatures(g_featureLayers[0], function(success,result)
+				{
+					expect(success).toBeTruthy();
+					expect(result.count).toBe(3);
+					done();					
+				});
+			},
+			function(error)
+			{
+				expect(true).toBeFalsy();
+				done();
+			});
+		});
+
+		async.it("update test features", function(done)
+		{
+			expect(g_featureLayers[0].graphics.length).toBe(3);
+
+			g1.geometry.y += 300;
+			g2.geometry.y += 100;
+			g3.geometry.y -= 200;
+			var updates = [g1,g2,g3];
+			g_featureLayers[0].applyEdits(null,updates,null,function(addResults,updateResults,deleteResults)
+			{
+				expect(updateResults.length).toBe(3);
+				expect(updateResults[0].success).toBeTruthy();
+				expect(updateResults[1].success).toBeTruthy();
+				expect(updateResults[2].success).toBeTruthy();
+				expect(g_featureLayers[0].graphics.length).toBe(3);
+				done();
+			},
+			function(error)
+			{
+				expect(true).toBeFalsy();
+				done();
+			});
+		});
+
+		async.it("delete test features", function(done)
+		{
+			expect(g_featureLayers[0].graphics.length).toBe(3);
+
+			var deletes = [g3];
+			g_featureLayers[0].applyEdits(null,null,deletes,function(addResults,updateResults,deleteResults)
+			{
+				expect(deleteResults.length).toBe(1);
+				expect(g_featureLayers[0].graphics.length).toBe(2);
+				done();
+			},
+			function(error)
+			{
+				expect(true).toBeFalsy();
+				done();
+			});
+		});
+	});
+});
+
 describe("Offline Editing", function()
 {
-	describe("Prepare Feature Layers", function()
-	{		
+	var g1,g2,g3;
+
+	async.it("clear feature Layers", function(done)
+	{
+		var count = 0;
+		function completedOne()
+		{
+			count += 1;
+			if(count==3)
+				done();
+		}
+		clearFeatureLayer( g_featureLayers[0], function(success,response)
+		{
+			expect(success).toBeTruthy();
+			var listener = g_featureLayers[0].on('update-end', function(){ listener.remove(); completedOne();})
+			g_featureLayers[0].refresh();
+			
+		});
+		clearFeatureLayer( g_featureLayers[1], function(success,response)
+		{
+			expect(success).toBeTruthy();
+			var listener = g_featureLayers[1].on('update-end', function(){ listener.remove(); completedOne();})
+			g_featureLayers[1].refresh();
+		});
+		clearFeatureLayer( g_featureLayers[2], function(success,response)
+		{
+			expect(success).toBeTruthy();
+			var listener = g_featureLayers[2].on('update-end', function(){ listener.remove(); completedOne();})
+			g_featureLayers[2].refresh();
+		});
 	});
 
-	describe("Go Offline", function()
+	async.it("add some features", function(done)
 	{
+		expect(g_offlineFeaturesManager.getOnlineStatus()).toBe(g_offlineFeaturesManager.ONLINE);
+
+		g1 = new g_modules.Graphic({"geometry":{"x":-105400,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+		g2 = new g_modules.Graphic({"geometry":{"x":-105600,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+		g3 = new g_modules.Graphic({"geometry":{"x":-105800,"y":5137000,"spatialReference":{"wkid":102100}},"attributes":{"symbolname":"Ground Zero","z":null,"additionalinformation":null,"eny":null,"datetimevalid":null,"datetimeexpired":null,"distance":null,"azimuth":null,"uniquedesignation":null,"x":null,"y":null}});
+
+		var adds = [g1,g2,g3];
+		g_featureLayers[0].applyEdits(adds,null,null,function(addResults,updateResults,deleteResults)
+		{
+			expect(addResults.length).toBe(3);
+			expect(g_featureLayers[0].graphics.length).toBe(3);
+			countFeatures(g_featureLayers[0], function(success,result)
+			{
+				expect(success).toBeTruthy();
+				expect(result.count).toBe(3);
+				done();
+			});
+		},
+		function(error)
+		{
+			expect(true).toBeFalsy();
+		});
 	});
 
-	describe("Update existing features", function()
+	async.it("go Offline", function(done)
 	{
+		expect(g_offlineFeaturesManager.getOnlineStatus()).toBe(g_offlineFeaturesManager.ONLINE);
+		g_offlineFeaturesManager.goOffline();
+		expect(g_offlineFeaturesManager.getOnlineStatus()).toBe(g_offlineFeaturesManager.OFFLINE);
+		done();
+	});
+
+	async.it("Update existing features", function(done)
+	{
+		expect(g_featureLayers[0].graphics.length).toBe(3);
+		expect(g_offlineFeaturesManager.getOnlineStatus()).toBe(g_offlineFeaturesManager.OFFLINE);
+
+		g1.geometry.y += 300;
+		g2.geometry.y += 100;
+		g3.geometry.y -= 200;
+		var updates = [g1,g2,g3];
+		g_featureLayers[0].applyEdits(null,updates,null,function(addResults,updateResults,deleteResults)
+		{
+			expect(updateResults.length).toBe(3);
+			expect(updateResults[0].success).toBeTruthy();
+			expect(updateResults[1].success).toBeTruthy();
+			expect(updateResults[2].success).toBeTruthy();
+			expect(g_featureLayers[0].graphics.length).toBe(3);
+			expect(g_editsStore.pendingEditsCount()).toBe(3);
+			done();
+		},
+		function(error)
+		{
+			expect(true).toBeFalsy();
+			done();
+		});
 	});
 	
-	describe("Delete existing features", function()
+	async.it("Update existing features again", function(done)
 	{
-	});
+		expect(g_featureLayers[0].graphics.length).toBe(3);
+		expect(g_offlineFeaturesManager.getOnlineStatus()).toBe(g_offlineFeaturesManager.OFFLINE);
 
-	describe("Add new features", function()
-	{
-	});
-
-	describe("Update new features", function()
-	{
+		g1.geometry.y += 50;
+		g2.geometry.y += 50;
+		g3.geometry.y -= 50;
+		var updates = [g1,g2,g3];
+		g_featureLayers[0].applyEdits(null,updates,null,function(addResults,updateResults,deleteResults)
+		{
+			expect(updateResults.length).toBe(3);
+			expect(updateResults[0].success).toBeTruthy();
+			expect(updateResults[1].success).toBeTruthy();
+			expect(updateResults[2].success).toBeTruthy();
+			expect(g_featureLayers[0].graphics.length).toBe(3);
+			expect(g_editsStore.pendingEditsCount()).toBe(6);
+			countFeatures(g_featureLayers[0], function(success,result)
+			{
+				expect(success).toBeTruthy();
+				expect(result.count).toBe(3);
+				done();
+			});
+		},
+		function(error)
+		{
+			expect(true).toBeFalsy();
+			done();
+		});
 	});
 	
-
-	describe("Delete new features", function()
+	async.it("Delete existing features", function(done)
 	{
+		expect(g_featureLayers[0].graphics.length).toBe(3);
+
+		var deletes = [g3];
+		g_featureLayers[0].applyEdits(null,null,deletes,function(addResults,updateResults,deleteResults)
+		{
+			expect(deleteResults.length).toBe(1);
+			expect(g_featureLayers[0].graphics.length).toBe(2);
+			expect(g_editsStore.pendingEditsCount()).toBe(7);
+			countFeatures(g_featureLayers[0], function(success,result)
+			{
+				expect(success).toBeTruthy();
+				expect(result.count).toBe(3); // still 3, the delete is still offline
+				done();
+			});
+		},
+		function(error)
+		{
+			expect(true).toBeFalsy();
+			done();
+		});
 	});
 
-	describe("Go Online", function()
+	async.it("Add new features", function(done)
 	{
+		expect("Not Implemented").toBe(null);
+		done();
 	});
-})
+
+	async.it("Update new features", function(done)
+	{
+		expect("Not Implemented").toBe(null);
+		done();
+	});
+	
+	async.it("Delete new features", function(done)
+	{
+		expect("Not Implemented").toBe(null);
+		done();
+	});
+
+	async.it("Go Online", function(done)
+	{
+		g_offlineFeaturesManager.goOnline(function()
+		{
+			expect(g_offlineFeaturesManager.getOnlineStatus()).toBe(g_offlineFeaturesManager.ONLINE);
+			expect(g_editsStore.pendingEditsCount()).toBe(0);
+			done();
+		});;
+		expect(g_offlineFeaturesManager.getOnlineStatus()).toBe(g_offlineFeaturesManager.RECONNECTING);
+	});
+});
