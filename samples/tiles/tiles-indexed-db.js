0295fc40c05e4970147f7fc8176093465547c06e
andygup <agup@esri.com>
2014-02-08 19:20:22 -0700

    better, restored max zoom level. Tile counts still aren't correct for the lower number zoom levels.

--- samples/tiles/tiles-indexed-db.js
+++ samples/tiles/tiles-indexed-db.js
@@ -182,30 +182,32 @@ require(["esri/map",
 		{
 			console.log('updating');
 			var zoomLevel = map.getLevel();
-			dojo.byId('currentLevel').value = zoomLevel;
-            dojo.byId('maxLevel').value = zoomLevel;
+            dojo.byId('currentLevel').value = zoomLevel;
 
-			var minLevel = parseInt(dojo.byId('minLevel').value);
-            var maxLevel = zoomLevel;
+            var minLevel = parseInt(dojo.byId('minLevel').value);
+            var maxLevel = parseInt(dojo.byId('maxLevel').value);
+
+            if( maxLevel > zoomLevel + 3 || maxLevel > basemapLayer.maxLevel)
+            {
+                maxLevel = Math.min(basemapLayer.maxLevel, zoomLevel + 3);
+                dojo.byId('maxLevel').value = maxLevel;
+            }
 
 			var totalEstimation = { tileCount:0, sizeBytes:0 }
 
 			domConstruct.empty('tile-count-table-body');
 
-            //NOTE: Number of tiles per zoom level will not change unless the map div changes size
-            var levelEstimation;
-
-            basemapLayer.getLevelEstimation(map.extent,minLevel,function(result){
-                levelEstimation = result;
-
+            basemapLayer.estimateTileSize(function(tileSize){
                 for(var level=minLevel; level<=maxLevel; level++)
                 {
+                    var levelEstimation = basemapLayer.getLevelEstimation(map.extent,level,tileSize);
+
                     totalEstimation.tileCount += levelEstimation.tileCount;
                     totalEstimation.sizeBytes += levelEstimation.sizeBytes;
 
-                    if( levelEstimation.tileCount > 1 && levelEstimation.tileCount < 5000)
+                    if( levelEstimation.tileCount > 1)
                     {
-                        var rowContent = [level, levelEstimation.tileCount, Math.round(levelEstimation.sizeBytes / 1024 / 1024 * 100) / 100 + " Mb"]
+                        var rowContent = [levelEstimation.level, levelEstimation.tileCount, Math.round(levelEstimation.sizeBytes / 1024 / 1024 * 100) / 100 + " Mb"]
                         rowContent = "<td>" + rowContent.join("</td><td>") + "</td>";
                         var tr = domConstruct.place("<tr>", dojo.byId('tile-count-table-body'),'last')
                         domConstruct.place(rowContent, tr,'last');
@@ -223,8 +225,35 @@ require(["esri/map",
                 rowContent = "<td><b>" + rowContent.join("</b></td><td><b>") + "</b></td>";
                 tr = domConstruct.place("<tr>", dojo.byId('tile-count-table-body'),'last')
                 domConstruct.place(rowContent, tr,'last');
-            });
+            })
 
+//                    for(var level=minLevel; level<=maxLevel; level++)
+//                    {
+//                        totalEstimation.tileCount += levelEstimation.tileCount;
+//                        totalEstimation.sizeBytes += levelEstimation.sizeBytes;
+//
+//                        if( levelEstimation.tileCount > 1 && levelEstimation.tileCount < 5000)
+//                        {
+//                            var rowContent = [level, levelEstimation.tileCount, Math.round(levelEstimation.sizeBytes / 1024 / 1024 * 100) / 100 + " Mb"]
+//                            rowContent = "<td>" + rowContent.join("</td><td>") + "</td>";
+//                            var tr = domConstruct.place("<tr>", dojo.byId('tile-count-table-body'),'last')
+//                            domConstruct.place(rowContent, tr,'last');
+//                        }
+//
+//                        if( totalEstimation.tileCount > 5000 )
+//                        {
+//                            var tr = domConstruct.place("<tr>", dojo.byId('tile-count-table-body'),'last')
+//                            domConstruct.place("<td colspan=4>...</td>", tr,'last');
+//                            break;
+//                        }
+//                    }
+//
+//                    rowContent = ["Total", totalEstimation.tileCount, Math.floor(totalEstimation.sizeBytes / 1024 / 1024 * 100)/100 + " Mb"];
+//                    rowContent = "<td><b>" + rowContent.join("</b></td><td><b>") + "</b></td>";
+//                    tr = domConstruct.place("<tr>", dojo.byId('tile-count-table-body'),'last')
+//                    domConstruct.place(rowContent, tr,'last');
+//                });
+//            })
 		}
 
 		function goOffline()
@@ -282,7 +311,7 @@ require(["esri/map",
 
 			/* launch offline preparation process */
 			var minLevel = parseInt(dojo.byId('minLevel').value);
-            var maxLevel = map.getLevel();
+            var maxLevel = map.getLevel();  //current zoom level sets max extent
 			basemapLayer.prepareForOffline(minLevel, maxLevel, map.extent, reportProgress);
 		}
 
