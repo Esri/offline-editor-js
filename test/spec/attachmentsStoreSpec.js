c50559a5754718b821142beaa62beb07591cb788
Javier Abadia <javi.abadia@gmail.com>
2014-04-05 01:40:41 +0200

    changed properties of stored attachments to mimic return values of queryAttachmentInfos REST API

--- test/spec/attachmentsStoreSpec.js
+++ test/spec/attachmentsStoreSpec.js
@@ -1,10 +1,10 @@
 "use strict";
 
 var testData = [
-	[ 1000, 1, "content of the file of attachment 1000 for feature 1"],
-	[ 1001, 1, "content of the file of attachment 1001 for feature 1"],
-	[ 1002, 2, "content of the file of attachment 1002 for feature 2"],
-	[ 1003, 3, "content of the file of attachment 1003 for feature 3"]
+	[ 1000, 1, { name: "attachment1000.txt", type: "text", size: "43", content: "content of the file of attachment 1000 for feature 1", url:""}],
+	[ 1001, 1, { name: "attachment1001.txt", type: "text", size: "43", content: "content of the file of attachment 1001 for feature 1", url:""}],
+	[ 1002, 2, { name: "attachment1002.txt", type: "text", size: "43", content: "content of the file of attachment 1002 for feature 2", url:""}],
+	[ 1003, 3, { name: "attachment1003.txt", type: "text", size: "43", content: "content of the file of attachment 1003 for feature 3", url:""}]
 ];
 
 describe("attachments store module", function()
@@ -46,7 +46,7 @@ describe("attachments store module", function()
 			g_attachmentsStore.getUsage(function(usage)
 			{
 				expect(usage).not.toBeNull();
-				expect(usage.sizeBytes).toBe(107);
+				expect(usage.sizeBytes).toBe(159);
 				expect(usage.attachmentCount).toBe(1);
 				done();
 			})
@@ -64,7 +64,7 @@ describe("attachments store module", function()
 			g_attachmentsStore.getUsage(function(usage)
 			{
 				expect(usage).not.toBeNull();
-				expect(usage.sizeBytes).toBe(107 * i);
+				expect(usage.sizeBytes).toBe(159 * i);
 				expect(usage.attachmentCount).toBe(i);
 				if( i == n)
 					done();
@@ -83,15 +83,15 @@ describe("attachments store module", function()
 			g_attachmentsStore.getAttachmentsByFeatureId(1, function(attachments)
 			{
 				expect(attachments.length).toBe(2);
-				expect(attachments[0].featureId).toBe(1);
-				expect(attachments[1].featureId).toBe(1);
-				expect(attachments[0].attachmentFile).toContain("feature 1");
-				expect(attachments[1].attachmentFile).toContain("feature 1");
+				expect(attachments[0].objectId).toBe(1);
+				expect(attachments[1].objectId).toBe(1);
+				expect(attachments[0].content).toContain("feature 1");
+				expect(attachments[1].content).toContain("feature 1");
 				g_attachmentsStore.getAttachmentsByFeatureId(2, function(attachments)
 				{
 					expect(attachments.length).toBe(1);
-					expect(attachments[0].featureId).toBe(2);
-					expect(attachments[0].attachmentFile).toContain("feature 2");
+					expect(attachments[0].objectId).toBe(2);
+					expect(attachments[0].content).toContain("feature 2");
 					done();
 				});
 			});
