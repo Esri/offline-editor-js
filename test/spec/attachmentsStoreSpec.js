74d6ec5563313939718a4decde5c267d28c17e96
Javier Abadia <javi.abadia@gmail.com>
2014-04-05 16:59:59 +0200

    store featurelayerurl along with attachment, so that we can keep attachments for different feature layers in the same indexeddb objectstore

--- test/spec/attachmentsStoreSpec.js
+++ test/spec/attachmentsStoreSpec.js
@@ -1,10 +1,10 @@
 "use strict";
 
 var testData = [
-	[ 1000, 1, { name: "attachment1000.txt", type: "text", size: "43", content: "content of the file of attachment 1000 for feature 1", url:""}],
-	[ 1001, 1, { name: "attachment1001.txt", type: "text", size: "43", content: "content of the file of attachment 1001 for feature 1", url:""}],
-	[ 1002, 2, { name: "attachment1002.txt", type: "text", size: "43", content: "content of the file of attachment 1002 for feature 2", url:""}],
-	[ 1003, 3, { name: "attachment1003.txt", type: "text", size: "43", content: "content of the file of attachment 1003 for feature 3", url:""}]
+	[ "layer1", 1000, 1, { name: "attachment1000.txt", type: "text", size: "43", content: "content of the file of attachment 1000 for feature 1 of layer1", url:""}],
+	[ "layer1", 1001, 1, { name: "attachment1001.txt", type: "text", size: "43", content: "content of the file of attachment 1001 for feature 1 of layer1", url:""}],
+	[ "layer1", 1002, 2, { name: "attachment1002.txt", type: "text", size: "43", content: "content of the file of attachment 1002 for feature 2 of layer1", url:""}],
+	[ "layer2", 1003, 1, { name: "attachment1003.txt", type: "text", size: "43", content: "content of the file of attachment 1003 for feature 1 of layer2", url:""}]
 ];
 
 describe("attachments store module", function()
@@ -39,7 +39,7 @@ describe("attachments store module", function()
 
 	async.it("store one attachment", function(done)
 	{
-		g_attachmentsStore.store(testData[0][0],testData[0][1],testData[0][2], function(success)
+		g_attachmentsStore.store(testData[0][0],testData[0][1],testData[0][2],testData[0][3], function(success)
 		{
 			expect(success).toBeTruthy();
 			g_attachmentsStore.getUsage(function(usage)
@@ -66,30 +66,36 @@ describe("attachments store module", function()
 				if( i == n)
 					done();
 				else
-					g_attachmentsStore.store(testData[i][0],testData[i][1],testData[i][2], addAttachment);
+					g_attachmentsStore.store(testData[i][0],testData[i][1],testData[i][2],testData[i][3], addAttachment);
 			})
 		};
-		g_attachmentsStore.store(testData[i][0],testData[i][1],testData[i][2], addAttachment);
+		g_attachmentsStore.store(testData[i][0],testData[i][1],testData[i][2],testData[i][3], addAttachment);
 	});
 
 	async.it("query attachments of a feature", function(done)
 	{
-		g_attachmentsStore.getAttachmentsByFeatureId(300, function(attachments)
+		g_attachmentsStore.getAttachmentsByFeatureId("layer1", 300, function(attachments)
 		{
 			expect(attachments.length).toBe(0);
-			g_attachmentsStore.getAttachmentsByFeatureId(1, function(attachments)
+			g_attachmentsStore.getAttachmentsByFeatureId("layer1", 1, function(attachments)
 			{
 				expect(attachments.length).toBe(2);
 				expect(attachments[0].objectId).toBe(1);
 				expect(attachments[1].objectId).toBe(1);
 				expect(attachments[0].content).toContain("feature 1");
 				expect(attachments[1].content).toContain("feature 1");
-				g_attachmentsStore.getAttachmentsByFeatureId(2, function(attachments)
+				g_attachmentsStore.getAttachmentsByFeatureId("layer1", 2, function(attachments)
 				{
 					expect(attachments.length).toBe(1);
 					expect(attachments[0].objectId).toBe(2);
 					expect(attachments[0].content).toContain("feature 2");
-					done();
+					g_attachmentsStore.getAttachmentsByFeatureId("layer2", 1, function(attachments)
+					{
+						expect(attachments.length).toBe(1);
+						expect(attachments[0].objectId).toBe(1);
+						expect(attachments[0].content).toContain("feature 1");
+						done();
+					});
 				});
 			});
 		});
