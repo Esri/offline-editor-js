6c599c45805ff902d1f17671d929b7bebf1c67d7
Andy Gup <agup2001@msn.com>
2014-05-22 17:11:33 -0600

    initial spec

--- test/spec/tpkLayerSpec.js
+++ test/spec/tpkLayerSpec.js
@@ -1,1 +1,33 @@
+"use strict";
 
+describe("TPKLayer module", function(){
+    var async = new AsyncSpec(this);
+    var tilesEntries = null;
+
+    console.log("Init tests...")
+
+    async.it("Validate TPKLayer init", function(done)
+    {
+        expect(tpkLayer).toEqual(jasmine.any(Object));
+        expect(tpkLayer.store).toEqual(jasmine.any(Object));
+        done();
+    });
+
+    async.it("Unzip TPK file", function(done){
+        var blob = FILE;
+        zip.createReader(new zip.BlobReader(blob), function (zipReader) {
+            zipReader.getEntries(function (entries) {
+
+                tilesEntries = entries;
+                console.log("DONE unzipping")
+                expect(entries.length).toBeGreaterThan(0)
+                done();
+                zipReader.close(function(evt){
+                    console.log("Done reading zip file.")
+                })
+            }, function (err) {
+                alert("There was a problem reading the file!: " + err);
+            })
+        }.bind(this))
+    })
+})
