5e43ced4b716ae7ae76534eb4c5fc5d7f680cca8
Andy Gup <agup2001@msn.com>
2015-04-07 17:19:05 -0600

    v2.5.1

--- dist/offline-tpk-src.js
+++ dist/offline-tpk-src.js
@@ -1,4 +1,4 @@
-/*! offline-editor-js - v2.5 - 2015-03-27
+/*! offline-editor-js - v2.5.1 - 2015-04-07
 *   Copyright (c) 2015 Environmental Systems Research Institute, Inc.
 *   Apache License*/
 /**
